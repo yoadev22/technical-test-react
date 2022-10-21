fa371279032b43998b08f9b42a1ecd0c4ae062d0
Simon Chan <simonkmc10@gmail.com>
2022-10-21 11:23:44 +0800

    initial setup

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,8 +1,19 @@
-const imgs = [
-    "https://www.publicdomainpictures.net/pictures/30000/velka/sunset-in-harbor.jpg",
-    "https://www.publicdomainpictures.net/pictures/80000/velka/sunrise-1394618133IlJ.jpg",
-    "https://www.publicdomainpictures.net/pictures/40000/velka/rural-field-1363007014cis.jpg",
-    "https://as2.ftcdn.net/v2/jpg/01/26/21/61/1000_F_126216157_NMcUhK1iOgw2asroRaX0686iYgjbdMNr.jpg",
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const IMAGE_SET = [
+    {
+        id: 1,
+        url: "https://www.publicdomainpictures.net/pictures/30000/velka/sunset-in-harbor.jpg",
+    },
+    {
+        id: 2,
+        url: "https://www.publicdomainpictures.net/pictures/40000/velka/rural-field-1363007014cis.jpg",
+    },
+    {
+        id: 3,
+        url: "https://as2.ftcdn.net/v2/jpg/01/26/21/61/1000_F_126216157_NMcUhK1iOgw2asroRaX0686iYgjbdMNr.jpg",
+    },
 ];
 
 export function ImageSlider() {
