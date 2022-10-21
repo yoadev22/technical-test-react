953eb801285dc31f48ff1986b1c5cae76cbe95fd
Simon Chan <simonkmc10@gmail.com>
2022-10-21 10:42:28 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ImageSlider } from "./ImageSlider";
+
+function App() {
+    return (
+        <div className="App">
+            <ImageSlider />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ImageSlider.jsx
@@ -0,0 +1,14 @@
+const imgs = [
+    "https://www.publicdomainpictures.net/pictures/30000/velka/sunset-in-harbor.jpg",
+    "https://www.publicdomainpictures.net/pictures/80000/velka/sunrise-1394618133IlJ.jpg",
+    "https://www.publicdomainpictures.net/pictures/40000/velka/rural-field-1363007014cis.jpg",
+    "https://as2.ftcdn.net/v2/jpg/01/26/21/61/1000_F_126216157_NMcUhK1iOgw2asroRaX0686iYgjbdMNr.jpg",
+];
+
+export function ImageSlider() {
+    return (
+        <div>
+            <img src="" alt="" />
+        </div>
+    );
+}
